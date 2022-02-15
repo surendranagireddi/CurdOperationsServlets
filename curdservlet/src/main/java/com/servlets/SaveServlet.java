bdb6981b19cc6b3b20932239aa39de8a670d137e
hpadmin <hpadmin@WIN-JP122Q4MUMK>
2022-02-15 13:20:11 +0530

    first commit

---
+++ curdservlet/src/main/java/com/servlets/SaveServlet.java
@@ -0,0 +1,46 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bean.Emp;
+import com.dao.EmpDao;
+
+@WebServlet("/SaveServlet") 
+public class SaveServlet extends HttpServlet {
+	
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response)   
+	         throws ServletException, IOException {  
+	        response.setContentType("text/html");  
+	        PrintWriter out=response.getWriter();  
+	          
+	        String name=request.getParameter("name");  
+	        String password=request.getParameter("password");  
+	        String email=request.getParameter("email");  
+	        String country=request.getParameter("country");  
+	        
+	        Emp e =new Emp();
+	        e.setName(name);
+	        e.setPassword(password);
+	        e.setEmail(email);
+	        e.setCountry(country);
+	        int status = EmpDao.Save(e);
+	        if(status>0){  
+	            out.print("<p>Record saved successfully!</p>");  
+	            request.getRequestDispatcher("index.html").include(request, response);  
+	        }else{  
+	            out.println("Sorry! unable to save record");  
+	        }  
+	          
+	        out.close();  
+	    }  
+	
+
+  }
+
